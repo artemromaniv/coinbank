b51b2e78ff763fcc98c173a8dc18fec39b139bc3
Artem Romaniv <artemromaniv@yahoo.com>
2022-09-20 02:06:16 -0700

    add .env.example

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -1,6 +1,9 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
+    // When we bootstrap React App with create-react-app we use environment variables by typing: process.env,<VARIABLE_NAME>
+    // but when we are using VIte build tool like in this project we do it with: import.meta.env.<VARIABLE_NAME>
+    
     'x-rapidapi-host': import.meta.env.VITE_CRYPTO_RAPIDAPI_HOST ='coinranking1.p.rapidapi.com'
     ,
     'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY,
