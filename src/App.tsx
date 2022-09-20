38de758f96c0cdbb26ed9efaeee165c692d86441
Artem Romaniv <artemromaniv@yahoo.com>
2022-09-20 04:53:07 -0700

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Cryptocurrencies from './components/Cryptocurrencies'
+import Sidebar from './components/Sidebar'
+import Exchanges from './components/Exchanges'
+import Markets from './components/Markets'
 
 const  App = () => {
 
   return (
     <div className="App">
-      
+      <div className="sidebar-container">
+        <Sidebar/>
+      </div>
+      <div className="main">
+        <Routes>
+          <Route path= '/' element = {<Cryptocurrencies/>} />
+          <Route path= '/exchanges' element = {<Exchanges/>} />
+          <Route path= '/markets' element = {<Markets/>} />
+        </Routes>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,36 @@
+import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+      <div className="logo-container">
+        <Link to={'/'} style = {{textDecoration: 'none'}} >
+          <span className="logo">
+            Coinbank
+          </span>
+        </Link>
+      </div>
+
+      <div className="nav-container">
+        <div className="">
+          <NavLink to={'/'}>
+            Cryptocurrencies
+          </NavLink>
+        </div>
+        <div className="">
+          <NavLink to={'/exchanges'}>
+            Exchanges
+          </NavLink>
+        </div>
+        <div className="">
+          <NavLink to={'/markets'}>
+            Markets
+          </NavLink>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
