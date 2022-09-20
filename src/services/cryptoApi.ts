7e2866c5e77b0d30dbd8e338bff5995329dc036d
Artem Romaniv <artemromaniv@yahoo.com>
2022-09-20 01:59:13 -0700

    fixing environment variables, providing store to our project

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -1,15 +1,16 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
-    'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
-    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+    'x-rapidapi-host': import.meta.env.VITE_CRYPTO_RAPIDAPI_HOST ='coinranking1.p.rapidapi.com'
+    ,
+    'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY,
   };
 
 const createRequest = (url:string) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
     reducerPath:'cryptoApi',
-    baseQuery:fetchBaseQuery({baseUrl: process.env.REACT_APP_CRYPTO_API_URL}),
+    baseQuery:fetchBaseQuery({baseUrl: import.meta.env.VITE_CRYPTO_API_URL}),
     endpoints:(builder) => ({
         getCryptoCurrency:builder.query({
             query:(count) => createRequest(`/coins?limit=${count}`)
