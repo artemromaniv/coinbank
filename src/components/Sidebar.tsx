aeaf8c1a074b6d78346b6cf600029e0a6020db81
Artem Romaniv <artemromaniv@yahoo.com>
2022-09-23 04:36:43 -0700

    add navigation icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router-dom"
 import { Link } from "react-router-dom"
-
+import { IconArrowsExchange,Icon3dCubeSphere, IconBuildingBank } from "@tabler/icons"
 const styles = {
   logo_container:'p-5',
   logo:'text-4xl',
   nav_container:'m-0 absolute top-1/2 -translate-y-1/2 ',
-  nav:'flex flex-col gap-14'
+  nav:'flex flex-col gap-14 bg-[#15181C] px-6 py-14',
+  nav_link_active:'p-0.5 bg-yellow-500',
+  nav_link_inactive:'p-0.5 bg-red-500'
 }
 
 const Sidebar = () => {
@@ -22,18 +24,18 @@ const Sidebar = () => {
       <div className={styles.nav_container}>
         <div className={styles.nav} >
           <div>
-            <NavLink to={'/'} className = {(isActive) => (isActive ? 'nav-link-active' : 'nav-link-inactive')} >
-              Cryptocurrencies
+            <NavLink to={'/'} className = {(isActive) => (isActive ? styles.nav_link_active : styles.nav_link_inactive)} >
+              <IconBuildingBank/>
             </NavLink>
           </div>
           <div>
-            <NavLink to={'/exchanges'} className = {(isActive) => (isActive ? 'nav-link-active' : 'nav-link-inactive')} >
-              Exchanges
+            <NavLink to={'/exchanges'} className = {(isActive) => (isActive ? styles.nav_link_active : styles.nav_link_inactive)} >
+              <IconArrowsExchange/>
             </NavLink>
           </div>
           <div>
-            <NavLink to={'/markets'} className = {(isActive) => (isActive ? 'nav-link-active' : 'nav-link-inactive')} >
-              Markets
+            <NavLink to={'/markets'} className = {(isActive) => (isActive ? styles.nav_link_active : styles.nav_link_inactive)} >
+              <Icon3dCubeSphere/>
             </NavLink>
           </div>
         </div>
