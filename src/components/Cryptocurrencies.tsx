53bbd1c0e015ab30133132384db9da3dae629a7c
Artem <artemromaniv@yahoo.com>
2022-10-03 05:00:12 -0700

    rebuild currency table

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -2,14 +2,14 @@ import {useState,useEffect} from 'react'
 import { useGetCryptoCurrencyQuery } from '../services/cryptoApi'
 
 const styles = {
-  wrapper:'w-full h-full flex flex-col-reverse lg:flex-row md:flex-col-reverse',
+  wrapper:'w-full h-full flex flex-col-reverse lg:flex-row md:flex-col-reverse ',
   table_container:'lg:basis-3/5 overflow-y-auto ',
-  table:'w-full text-sm  ',
-  table_head:'px-4 py-2 font-medium text-left text-white whitespace-nowrap',
-  table_body:'divide-gray-200',
-  table_row:'mt-2 bg-[#15181C]',
-  table_div:'px-4 py-2 text-white whitespace-nowrap',
-  table_icon:'w-7 h-7',
+  table_head:'flex flex-row',
+  table_left_cols:'basis-1/2 inline-grid grid-cols-4 items-center',
+  table_right_cols:'basis-1/2 inline-grid grid-cols-3 items-center',
+  table_body:'bg-gray-900 mt-4 rounded-2xl p-2',
+  table_row:'flex flex-row w-full',
+  coin_icon:'w-8 h-8',
   banner_container:'lg:basis-2/5',
 
 }
@@ -50,33 +50,38 @@ const Cryptocurrencies = () => {
         : isLoading ? (<h1>I'm loading</h1>) : (
           <div className={styles.wrapper} >
             <div className={styles.table_container}>
-              <table className={styles.table}>
-                <thead>
-                  <tr className='items-center'>
-                    <th className={styles.table_head} >#</th>
-                    <th className={styles.table_head} >icon</th>
-                    <th className={styles.table_head}>Name</th>
-                    <th className={styles.table_head}>Symbol</th>
-                    <th className={styles.table_head}>Price</th>
-                    <th className={styles.table_head}>Change</th>
-                  </tr>
-                </thead>
-                <tbody className={styles.table_body} >
-                  {currencies?.map((coin:Currencies) => (
-                    <tr key = {coin.uuid} className = {styles.table_row}>
-                        <td className={styles.table_div} >{coin.rank}</td>
-                        <td className={styles.table_div}>
-                          <img className={styles.table_icon} src={coin.iconUrl}></img>
-                        </td>
-                        <td className={styles.table_div} >{coin.name}</td>
-                        <td className={styles.table_div} >{coin.symbol}</td>
-                        <td className={styles.table_div} >{coin.price}</td>
-                        <td className={styles.table_div} >{coin.change}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-
+              <div className='p-2'>
+                <ul className={styles.table_head}>
+                  <li className={styles.table_left_cols}>
+                    <span>#</span>
+                    <span>{' '}</span>
+                    <span>Name</span>
+                    <span>{''}</span>
+                  </li>
+                  <li className = {styles.table_right_cols}>
+                    <span>Price</span>
+                    <span>Market Cap</span>
+                    <span>Change</span>
+                  </li>
+                </ul>
+              </div>
+              {currencies?.map((coin:Currencies) => (
+                <div key={coin.uuid} className = {styles.table_body}>
+                  <ul className={styles.table_row}>
+                    <li className={styles.table_left_cols}>
+                      <span>{coin.rank}.</span>
+                      <img src={coin.iconUrl} alt={coin.symbol} className = {styles.coin_icon} />
+                      <span>{coin.name}</span>
+                      <span>{coin.symbol}</span>
+                    </li>
+                    <li className={styles.table_right_cols} >
+                      <span>{coin.price}</span>
+                      <span>{coin.marketCap}</span>
+                      <span>{coin.change}</span>
+                    </li>
+                  </ul>
+                </div>
+              ))}
             </div>
             <div className={styles.banner_container}>
               <h1>{stats?.totalCoins}</h1>
